68b532412af6c6c9b9ab243fa99e3b51776e6016
Richard Urunuela <richard@Mac-mini-de-Richard-2.local>
2017-10-06 17:57:02 +0200

    1 seul hexagone Ok

--- hexagone/PlateauScene.swift
+++ hexagone/PlateauScene.swift
@@ -14,23 +14,17 @@ import GameplayKit
 
 class Hexagone:SKSpriteNode {
     var edgeSize = 0
-
-    convenience init?(edgeSize:CGFloat) {
     
+    convenience init?(edgeSize:CGFloat) {
+        
         guard let texture = Hexagone.hexagoneTexture(edgeSize:CGFloat(edgeSize)) else {
             return nil
         }
         self.init(texture: texture, color:SKColor.clear, size: texture.size())
         self.edgeSize = Int(edgeSize)
-    
+        
         self.isUserInteractionEnabled = true
-        //center
-        //let Circle = SKShapeNode(circleOfRadius: 2 ) // Create circle
-        //Circle.position = CGPoint(x: x, y:  y)  // Center (given scene anchor point is 0.5 for x&y)
-        // Circle.strokeColor = SKColor.black
-        // Circle.glowWidth = 1.0
-        //Circle.fillColor = SKColor.orange
-        //self.addChild(Circle)
+        
         var scoreLabel = SKLabelNode(fontNamed: "Arial")
         scoreLabel.text = "0,0"
         scoreLabel.horizontalAlignmentMode = .center
@@ -39,10 +33,8 @@ class Hexagone:SKSpriteNode {
         scoreLabel.position = CGPoint(x:0, y: edgeSize/4)
         addChild(scoreLabel)
         
-        //
     }
     class func hexagoneTexture(edgeSize:CGFloat) -> SKTexture? {
-        // Add 1 to the height and width to ensure the borders are within the sprite
         let size = CGSize(width: CGFloat(edgeSize)  + 1.0, height: CGFloat(edgeSize) + 1.0)
         UIGraphicsBeginImageContext(size)
         
@@ -50,7 +42,7 @@ class Hexagone:SKSpriteNode {
             return nil
         }
         let rect = CGRect(x: edgeSize/2-3, y: edgeSize/2-3, width: 6, height: 6)
-
+        
         
         SKColor.lightGray.setFill()
         context.fillEllipse(in: rect)
@@ -58,13 +50,7 @@ class Hexagone:SKSpriteNode {
         
         
         let bezierPath = UIBezierPath()
-        //let offset:CGFloat = 0.5
-        let xcenter  = size.height / 2
-        let ycenter  = size.width / 2
-        //point top
-        //let xpos = [0.5,1,1,0.5,0,0,0.5]
-        //let ypos = [0,0.25,0.75,1,0.75,0.25,0]
-        //flat top
+        
         let ypos = [0.5,1,1,0.5,0,0,0.5]
         let xpos = [0,0.25,0.75,1,0.75,0.25,0]
         
@@ -81,13 +67,13 @@ class Hexagone:SKSpriteNode {
             bezierPath.addLine(to: CGPoint(x: CGFloat(xp), y: CGFloat(yp)))
             
         }
-      
+        
         bezierPath.fill()
         
         SKColor.red.setStroke()
         bezierPath.lineWidth = 2.0
         bezierPath.stroke()
-     
+        
         
         context.addPath(bezierPath.cgPath)
         let image = UIGraphicsGetImageFromCurrentImageContext()
@@ -103,7 +89,7 @@ class Hexagone:SKSpriteNode {
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         debugPrint(" CLick hexagone")
-    
+        
     }
     
     
@@ -169,11 +155,11 @@ class Grid:SKSpriteNode {
             let position = touch.location(in:self)
             let node = atPoint(position)
             
-                let x = size.width / 2 + position.x
-                let y = size.height / 2 - position.y
-                let row = Int(floor(x / blockSize))
-                let col = Int(floor(y / blockSize))
-                print("\(row) \(col)")
+            let x = size.width / 2 + position.x
+            let y = size.height / 2 - position.y
+            let row = Int(floor(x / blockSize))
+            let col = Int(floor(y / blockSize))
+            print("\(row) \(col)")
             
         }
     }
@@ -186,50 +172,26 @@ class PlateauScene: SKScene {
     private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-      
-       //self.label?.text = " HELLO"
-        self.label?.removeFromParent()
-        
         //GRID
         if let grid = Grid(blockSize: 20.0, rows:30, cols:30) {
             grid.position = CGPoint (x:frame.midX, y:frame.midY)
             //grid.position = CGPoint (x:frame.width / 8, y:-frame.height / 8)
             addChild(grid)
-            
-            /*let gamePiece = SKSpriteNode(imageNamed: "Spaceship")
-            gamePiece.setScale(0.0625)
-            gamePiece.position = grid.gridPosition(row: 1, col: 0)
-            grid.addChild(gamePiece)*/
-            
-            
         }
         if let hex = Hexagone(edgeSize: 80.0){
             hex.position = CGPoint (x:0, y:0)
-           addChild(hex)
-        }
-        if let hex = Hexagone(edgeSize: 80.0){
-            hex.position = CGPoint (x:-60, y:40)
-            addChild(hex)
-        }
-        if let hex = Hexagone(edgeSize: 80.0){
-            hex.position = CGPoint (x:60, y:40)
             addChild(hex)
         }
         
-        
-       
     }
     
-    // DrawGrid
     
     
-  
+    
+    
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-       
+        
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
